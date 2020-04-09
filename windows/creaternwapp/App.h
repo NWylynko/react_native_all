3eda2b9a0caf686f61ad7858b31699798ec2f78b
NWylynko <nick1014375@gmail.com>
2020-04-10 00:28:44 +0800

    initial

---
+++ windows/creaternwapp/App.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+
+
+namespace winrt::creaternwapp::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::creaternwapp::implementation
+
+
